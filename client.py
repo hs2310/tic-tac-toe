6b8a59dad1eda1917a715abab02d3a8c00b0c704
hs2310 <harshilshah186@gmail.com>
2020-07-25 19:23:08 -0700

    Simple Tic Tac Toe Created / Socket programming start

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((socket.gethostname() , 1234))
+
+while True:
+
+    msg = s.recv(128)
+
+    print(msg.decode("utf-8"))
---
+++ server.py
@@ -0,0 +1,84 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((socket.gethostname(), 1234))
+
+s.listen(5)
+
+board = [ [' ' , ' ' , ' '], [' ' , ' ' , ' '], [' ' , ' ' , ' ']]
+
+current_player = "O" 
+
+def printBoard():
+    print("",end="\t\t")
+    for i in range(3):
+        for j in range(3):
+            if j == 2 : print(board[i][j],end="\n\t\t")
+            else : print(board[i][j],end= "|")
+        if i != 2: print("-----", end="\n\t\t")
+
+def checkGame():
+    if board[0][0] == board[0][1] and board[0][2] == board[0][1] and board[0][0] != ' ': return True
+    elif board[1][0] == board[1][1] and board[1][2] == board[1][1] and board[1][0] != ' ': return True
+    elif board[2][0] == board[2][1] and board[2][2] == board[2][1] and board[2][0] != ' ': return True
+    elif board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[0][0] != ' ': return True
+    elif board[0][1] == board[1][1] and board[1][1] == board[2][1] and board[0][1] != ' ': return True
+    elif board[0][2] == board[1][2] and board[1][2] == board[2][1] and board[0][2] != ' ': return True
+    elif board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != ' ': return True
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[0][2] != ' ': return True
+    return False
+
+def isEmpty():
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == ' ':
+                return True
+    return False
+
+# while True:
+
+#     printBoard()
+
+#     x = int(input("\nEnter x coordinate ("+ current_player + ") : " ))
+#     y = int(input("Enter y coordinate ("+ current_player + ") : " ))
+    
+#     if board[x][y] == ' ': 
+#         board[x][y] = current_player
+#     else :
+#         print("Invalid Move")
+#         continue
+#     if checkGame() == True:
+#         print( "Player (" + current_player + ") Won ")
+#         break
+#     elif not isEmpty() :
+#         print( "It's a Tie"  )
+#         break
+    
+#     if current_player == 'O':
+#         current_player = 'X'
+#     else : current_player = 'O'
+
+players = dict()
+
+i = 0
+
+while True : 
+    if len(players) == 2 :
+        break
+    clientsocket,  address = s.accept() 
+    print(f"Connection from {address} has been established!!")
+    clientsocket.send(bytes("Welcome to the server!" , "utf-8"))
+    if i == 0 :
+        players["O"] = clientsocket
+    if i == 1 : 
+        players["X"] = clientsocket
+    i+=1
+
+print(players)
+
+players["O"].send(bytes("WELCOME YOU ARE PLAYER \"O\"" , "utf-8"))
+players["X"].send(bytes("WELCOME YOU ARE PLAYER \"X\"" , "utf-8"))
+
+players["0"].close()
+players["X"].close()
